767f6b2b3b6fe1ac461bfca69a07917551e66065
Franklin Kuba <franklinkuba7@gmail.com>
2024-08-17 12:33:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Git is a vcs we can use to track different version systems.
+// It is a decentralized version control system.
+
+/*
+// CVCS/////////////////////////////////////////////////////////////////
+// A central server will contain the codebase for all the engineers
+
+// Problem is if they are not close to a place to connect to the server, they can not work on the code.
+// If something bad happens and the server is damaged, the code is lost and nobody can work on it.
+
+DVCS///////////////////////////////////////////////////////////////////
+Do not have to be physically present. Can connect remotely.
+
+// No fear of losing the code.
+// Copy of the code stays in their system.
+
+// git init - initialize
+*/
